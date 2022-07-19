add7eeaa2bb69c84bb445d7c6e98b590de4dbe83
vunguyen <anhvunguyen1208@gmail.com>
2022-07-19 19:20:35 +0800

    Add adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,19 @@ import adapter from "@sveltejs/adapter-auto";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true
+    },
 
     // Override http methods in the Todo forms
     methodOverride: {
